0078b1fbdaaa26c3679ededb7a02658e2d22997d
david awad <me@davidaw.ad>
2022-02-24 00:02:31 -0500

    AUTO COMMIT: 20220224-000231

--- src/routes/purchase.js
+++ src/routes/purchase.js
@@ -39,12 +39,12 @@ router.post("/", async (req, res) => {
     return res.status(400).send("Bad Request");
   }
 
-  if (!isAddress(addr)) {
-    logger.error(`Request rejected. ${req.body.addr} is not a valid address`);
-    return res.status(400).send("Bad Request");
-  }
+  // if (!isAddress(addr)) {
+  //   logger.error(`Request rejected. ${addr} is not a valid address`);
+  //   return res.status(400).send("Bad Request");
+  // }
 
-  if (parseFloat(req.body.amount) <= 0) {
+  if (parseFloat(amount) <= 0) {
     logger.error("Request rejected. invalid amount");
     return res.status(400).send("Bad Request");
   }
